3a23c03dc307266c593a3842ce5a0772de2e0888
Neto Freitas <36890320+janetofreitas@users.noreply.github.com>
2022-10-27 01:20:40 +0000

    indo

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 print("Hello World")
 nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
-
+idade = int(input("Digite sua idade: "))
+sexo = 'M'
 
 
 dobro = int(idade)*2
 print(dobro)
+if (idade >= 18 and sexo == 'M'):
+ print('1')
+else:
+  print('2')
