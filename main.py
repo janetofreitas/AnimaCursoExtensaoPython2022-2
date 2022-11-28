19927c1ba3775376bdfa0021b89dd68d2f11a8b1
Neto Freitas <36890320+janetofreitas@users.noreply.github.com>
2022-11-28 04:18:55 +0000

    CE pyton

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-print("Hello World")
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-sexo = 'M'
+#Importa nosso arquivo Pessoa.py no diretório model
+from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
+
+#Exemplo de uso
+neto = Pessoa(1, "Neto Freitas")
+print(neto)
+
+#Quero mostrar só o nome
+print(neto.nome)
+
+print("DAQUI PRA FRENTE É ACESSO AO BANCO...")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
 
 
-dobro = int(idade)*2
-print(dobro)
-if (idade >= 18 and sexo == 'M'):
- print('1')
-else:
-  print('2')
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+novo = Pessoa(0, "Denzel Washington")
+print(novo)
+#Olha que simples...
+novo = pessoaDAO.save(novo)
+
+#consulta o banco de novo..
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
